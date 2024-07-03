eb29979214cc908c0466382a1416a4def28361ca
Ratan Gulati <ratangulati.dev@gmail.com>
2024-07-03 12:53:04 +0530

    added vercel configuration

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,12 @@ app.use(express.json());
 app.use("/api/v1", rootRouter);
 
 app.listen(3000);
+
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
