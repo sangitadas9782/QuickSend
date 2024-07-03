2ea58e69825023b01f1ba4258084fb8145600323
Ratan Gulati <ratangulati.dev@gmail.com>
2024-07-03 20:33:14 +0530

    updated  vercel configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,32 @@
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Signup } from "./pages/Signup";
-import { Signin } from "./pages/Signin";
-import { Dashboard } from "./pages/Dashboard";
-import { SendMoney } from "./pages/SendMoney";
-import { PrivateRoute } from "./components/PrivateRoute";
 import { AuthProvider } from "./context/AuthContext";
-import { Profile } from "./pages/Profile";
-import { TransactionHistory } from "./pages/TransactionHistory";
-import { Home } from "./pages/Home";
+import { PrivateRoute } from "./components/PrivateRoute";
+import { Loader } from "./components/Loader";
+
+const Home = lazy(() => import("./pages/Home"));
+const Signup = lazy(() => import("./pages/Signup"));
+const Signin = lazy(() => import("./pages/Signin"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const SendMoney = lazy(() => import("./pages/SendMoney"));
+const Profile = lazy(() => import("./pages/Profile"));
+const TransactionHistory = lazy(() => import("./pages/TransactionHistory"));
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/dashboard"element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-          <Route path="/send"element={<PrivateRoute><SendMoney /></PrivateRoute>} />
-          <Route path="/profile"element={<PrivateRoute><Profile /></PrivateRoute>} />
-          <Route path="/transactions"element={<PrivateRoute><TransactionHistory /></PrivateRoute>} />
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+            <Route path="/send" element={<PrivateRoute><SendMoney /></PrivateRoute>} />
+            <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+            <Route path="/transactions" element={<PrivateRoute><TransactionHistory /></PrivateRoute>} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </AuthProvider>
   );
