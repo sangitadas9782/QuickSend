7a41fd9daf471297210391d1b61cd52a1eff8679
Ratan Gulati <ratangulati.dev@gmail.com>
2024-07-03 20:35:00 +0530

    updated  vercel configuration

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,12 +1,13 @@
 import { Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import Loader from './Loader';
 
 export const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div><Loader /></div>;
   }
 
   if (!user) {
